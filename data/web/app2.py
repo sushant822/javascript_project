1a788d1f7cf899fc29428c95442ce5bce7496aa0
Sushant Deshpande <41443708+sushant822@users.noreply.github.com>
2020-09-19 12:53:40 -0600

    Connections working

--- data/web/app2.py
+++ data/web/app2.py
@@ -18,6 +18,22 @@ con = psycopg2.connect(
             password="1"
 )
 
+def calgary_data_fun():
+    cur = con.cursor()
+
+    # execute query
+    cur.execute("SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary_df AS cl JOIN score_df AS s ON cl.postal_code = s.postal_code JOIN coordinates_df AS coord ON s.postal_code = coord.postal_codes")
+
+    calgary_data = cur.fetchall()
+    # print(calgary_data)
+
+    # close cursor
+    cur.close()
+
+    # close the connection
+    con.close()
+
+    return calgary_data
 
 @app.route("/")
 def home():
@@ -29,29 +45,25 @@ def home():
     cur.execute("SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary_df AS cl JOIN score_df AS s ON cl.postal_code = s.postal_code JOIN coordinates_df AS coord ON s.postal_code = coord.postal_codes")
 
     calgary_data = cur.fetchall()
-    print(calgary_data)
-
-    # for r in calgary_data:
-    #     print(f'house on {r[1]} costs ${r[0]} and a walkscore of {r[3]}')
-
+    # print(calgary_data)
 
     # close cursor
-    cur.close()
-
+    #cur.close()
 
     # close the connection
-    con.close()
+    #con.close()
 
     # Return template and data
     return render_template("index.html", calgary=[i for i in calgary_data])
 
 
 
-@app.route("/api/v1.0/calgaryRealEstate")
+@app.route("/jsonified")
 def calgary_data():
     """Return the Calgary data as json"""
-
-    return jsonify(calgary_data)
+    calgary = calgary_data_fun()
+    calgary = calgary[:15]
+    return jsonify(calgary)
 
 
 if __name__ == "__main__":

7ec9b1f3715c8efa09828a9306bf1d2b4d71ba86
dayothompson <dayo.thompson@yahoo.com>
2020-10-01 16:21:58 -0400

    push

--- README.md
+++ README.md
@@ -49,6 +49,57 @@ Screenshots of how a user can select visualisation options: Update with pics aft
 
 Expanding to other cities in Canada
 
+## Homepage
+![Screen Shot 2020 10 01 At 3.54.57 PM](images/Screen%20Shot%202020-10-01%20at%203.54.57%20PM.png)
+
+![Screen Shot 2020 10 01 At 3.55.20 PM](images/Screen%20Shot%202020-10-01%20at%203.55.20%20PM.png)
+
+## Cluster map
+![Screen Shot 2020 10 01 At 3.58.05 PM](images/Screen%20Shot%202020-10-01%20at%203.58.05%20PM.png)
+
+![Screen Shot 2020 10 01 At 3.58.29 PM](images/Screen%20Shot%202020-10-01%20at%203.58.29%20PM.png)
+
+![Screen Shot 2020 10 01 At 3.58.47 PM](images/Screen%20Shot%202020-10-01%20at%203.58.47%20PM.png)
+
+![Screen Shot 2020 10 01 At 3.59.04 PM](images/Screen%20Shot%202020-10-01%20at%203.59.04%20PM.png)
+
+
+## Heat Map
+
+![Screen Shot 2020 10 01 At 3.59.25 PM](images/Screen%20Shot%202020-10-01%20at%203.59.25%20PM.png)
+
+
+## Scatter Plot
+
+### Price vs (Walk Score, Transit Score, Bike Score)
+
+![Screen Shot 2020 10 01 At 4.01.03 PM](images/Screen%20Shot%202020-10-01%20at%204.01.03%20PM.png)
+
+![Screen Shot 2020 10 01 At 4.01.29 PM](images/Screen%20Shot%202020-10-01%20at%204.01.29%20PM.png)
+
+![Screen Shot 2020 10 01 At 4.02.21 PM](images/Screen%20Shot%202020-10-01%20at%204.02.21%20PM.png)
+
+
+### Price vs Property Area
+![Screen Shot 2020 10 01 At 4.02.36 PM](images/Screen%20Shot%202020-10-01%20at%204.02.36%20PM.png)
+
+
+
+### Property Area vs (Walk Score, Transit Score, Bike Score)
+![Screen Shot 2020 10 01 At 4.03.05 PM](images/Screen%20Shot%202020-10-01%20at%204.03.05%20PM.png)
+
+![Screen Shot 2020 10 01 At 4.03.23 PM](images/Screen%20Shot%202020-10-01%20at%204.03.23%20PM.png)
+
+![Screen Shot 2020 10 01 At 4.03.39 PM](images/Screen%20Shot%202020-10-01%20at%204.03.39%20PM.png)
+
+
+## Density Graph
+![Screen Shot 2020 10 01 At 4.04.14 PM](images/Screen%20Shot%202020-10-01%20at%204.04.14%20PM.png)
+
+![Screen Shot 2020 10 01 At 4.04.34 PM](images/Screen%20Shot%202020-10-01%20at%204.04.34%20PM.png)
+
+![Screen Shot 2020 10 01 At 4.04.49 PM](images/Screen%20Shot%202020-10-01%20at%204.04.49%20PM.png)
+
 
 ## Authors and Acknowledgements
 
--- data/web/app.py
+++ data/web/app.py
@@ -15,14 +15,14 @@ con = psycopg2.connect(
             host="localhost",
             database="realestate_db",
             user="postgres",
-            password="1"
+            password="123"
 )
 
 def calgary_data_fun():
     cur = con.cursor()
 
     # execute query
-    cur.execute("SELECT json_agg(t) FROM (SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary_df AS cl JOIN score_df AS s ON cl.postal_code = s.postal_code JOIN coordinates_df AS coord ON s.postal_code = coord.postal_codes WHERE cl.price < 1000000 AND cl.property_area < 4000 AND s.walk_score > 5 AND s.bike_score > 0 AND s.transit_score > 0)t")
+    cur.execute("SELECT json_agg(t) FROM (SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary AS cl JOIN score AS s ON cl.postal_code = s.postal_code JOIN coordinates AS coord ON s.postal_code = coord.postal_codes WHERE cl.price < 1000000 AND cl.property_area < 4000 AND s.walk_score > 5 AND s.bike_score > 0 AND s.transit_score > 0)t")
 
     #cur.execute("SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary_df AS cl JOIN score_df AS s ON cl.postal_code = s.postal_code JOIN coordinates_df AS coord ON s.postal_code = coord.postal_codes")
 
@@ -41,7 +41,7 @@ def bar_fun():
     cur = con.cursor()
 
     # execute query
-    cur.execute("SELECT json_agg(t) FROM (SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary_df AS cl JOIN score_df AS s ON cl.postal_code = s.postal_code JOIN coordinates_df AS coord ON s.postal_code = coord.postal_codes WHERE cl.price < 1000000 AND cl.property_area < 4000 AND cl.property_type IS NOT null)t")
+    cur.execute("SELECT json_agg(t) FROM (SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary AS cl JOIN score AS s ON cl.postal_code = s.postal_code JOIN coordinates AS coord ON s.postal_code = coord.postal_codes WHERE cl.price < 1000000 AND cl.property_area < 4000 AND cl.property_type IS NOT null)t")
 
     #cur.execute("SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary_df AS cl JOIN score_df AS s ON cl.postal_code = s.postal_code JOIN coordinates_df AS coord ON s.postal_code = coord.postal_codes")
 
@@ -63,7 +63,7 @@ def home():
     cur = con.cursor()
 
     # execute query
-    cur.execute("SELECT json_agg(t) FROM (SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary_df AS cl JOIN score_df AS s ON cl.postal_code = s.postal_code JOIN coordinates_df AS coord ON s.postal_code = coord.postal_codes WHERE cl.price < 1000000 AND cl.property_area < 4000)t")
+    cur.execute("SELECT json_agg(t) FROM (SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary AS cl JOIN score AS s ON cl.postal_code = s.postal_code JOIN coordinates AS coord ON s.postal_code = coord.postal_codes WHERE cl.price < 1000000 AND cl.property_area < 4000)t")
 
     #cur.execute("SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary_df AS cl JOIN score_df AS s ON cl.postal_code = s.postal_code JOIN coordinates_df AS coord ON s.postal_code = coord.postal_codes")
     
@@ -113,7 +113,7 @@ def data():
     cur = con.cursor()
 
     # execute query
-    cur.execute("SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary_df AS cl JOIN score_df AS s ON cl.postal_code = s.postal_code JOIN coordinates_df AS coord ON s.postal_code = coord.postal_codes WHERE cl.price < 1000000 AND cl.property_area < 4000")
+    cur.execute("SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary AS cl JOIN score AS s ON cl.postal_code = s.postal_code JOIN coordinates AS coord ON s.postal_code = coord.postal_codes WHERE cl.price < 1000000 AND cl.property_area < 4000")
 
     #cur.execute("SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary_df AS cl JOIN score_df AS s ON cl.postal_code = s.postal_code JOIN coordinates_df AS coord ON s.postal_code = coord.postal_codes")
     
